3c9a07444b295203f259a767ce9cf32fa0cd77de
Esaú Peralta <esau.opr@gmail.com>
2019-05-01 21:09:27 -0500

    remove spaces

--- src/permutacion.cpp
+++ src/permutacion.cpp
@@ -2,14 +2,14 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
 typedef long long int LL;
 
 LL fact[25];
 
-void permu(string s, LL x, int l)
-{
+void permu(string s, LL x, int l) {
 	if(l<0) return;
 	LL p = x/fact[l];
 	printf("%c", s[p]);
@@ -17,18 +17,17 @@ void permu(string s, LL x, int l)
 	permu(s,x%fact[l],l-1);
 }
 
-int main()
-{
+int main() {
 
 	fact[0]=1;
 	for(int i=1; i<21; i++)
 		fact[i] = fact[i-1]*i;
 	string s = "abcd";
-	
+
 	LL x;
-	
+
 	scanf("%lld", &x);
-	
+
 	permu(s,x % fact[s.size()],s.size()-1);
 	puts("");
 
